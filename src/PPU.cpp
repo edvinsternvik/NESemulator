00005d560a7efce1d2b8b9e64e3a05f9894bcb72
RedE01 <e.sternvik@gmail.com>
2021-02-15 20:52:09 +0100

    Implement PPU registers

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -1,6 +1,117 @@
 #include "PPU.h"
 #include "Buss.h"
 
+PPU::PPU()
+    : m_scanline(261), m_cycle(0), m_nametableOffset(0), m_nmi(0) {
+}
+
+void PPU::clock() {
+}
+
 void PPU::registerBuss(Buss* buss) {
     m_buss = buss;
+}
+
+bool PPU::nmi() {
+    if(m_nmi) {
+        m_nmi = false;
+        return true;
+    }
+    return false;
+}
+
+uint8_t PPU::read(const uint16_t& address) {
+    if(m_buss) {
+        return m_buss->read(address);
+    }
+    return 0;
+}
+
+void PPU::write(const uint16_t& address, const uint8_t& data) {
+    if(m_buss) {
+        m_buss->write(address, data);
+    }
+}
+
+uint8_t PPUregisters::read(const uint16_t& address) {
+    uint8_t localAddr = address % 8;
+    uint8_t data = 0;
+
+    switch(localAddr) {
+    case 0: // Write only
+        break;
+    case 1: // Write only
+        break;
+    case 2:
+        data = m_ppu->PPUstatus.reg & 0xE0; // Low 5 bits are unused
+        m_ppu->PPUstatus.verticalBlank = 0;
+        m_addressLatch = false;
+        break;
+    case 3: // Write only
+        break;
+    case 4:
+        break;
+    case 5: // Write only
+        break;
+    case 6: // Write only
+        break;
+    case 7:
+        data = m_dataBuffer;
+        m_dataBuffer = m_ppu->read(m_ppu->m_vramAddress);
+
+        // If reading palette data ($3F00 - $3FFF), read directly, bypass the data buffer
+        if(m_ppu->m_vramAddress >= 0x3F00) data = m_dataBuffer;
+
+        if(m_ppu->PPUctrl.vramInc) m_ppu->m_vramAddress += 32;
+        else m_ppu->m_vramAddress += 1;
+        break;
+    }
+
+    return data;
+}
+
+void PPUregisters::write(const uint16_t& address, const uint8_t& data) {
+    uint8_t localAddr = address % 8;
+
+    switch(localAddr) {
+    case 0:
+        m_ppu->PPUctrl.reg = data;
+        break;
+    case 1:
+        m_ppu->PPUmask.reg = data;
+        break;
+    case 2: // Read only
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        if(!m_addressLatch) {
+            // Write horizontal scroll
+            m_addressLatch = true;
+        }
+        else {
+            // Write vertical scroll
+            m_addressLatch = true;
+        }
+        break;
+    case 6:
+        if(!m_addressLatch) {
+            m_ppu->m_tvramAddress = (uint16_t)((data & 0x3F) << 8 | (m_ppu->m_tvramAddress & 0x00FF));
+            m_addressLatch = true;
+        }
+        else {
+            m_ppu->m_tvramAddress = (m_ppu->m_tvramAddress & 0xFF00) | data;
+            m_ppu->m_vramAddress = m_ppu->m_tvramAddress;
+            m_addressLatch = false;
+        }
+        break;
+    case 7:
+        m_ppu->write(m_ppu->m_vramAddress, data);
+
+        if(m_ppu->PPUctrl.vramInc) m_ppu->m_vramAddress += 32;
+        else m_ppu->m_vramAddress += 1;
+        break;
+    }
 }
--- src/PPU.h
+++ src/PPU.h
@@ -7,27 +7,88 @@ class Buss;
 
 class PPU {
 public:
-    void registerBuss(Buss* buss);
+    PPU();
+    void clock();
 
-public:
-    uint8_t registers[8] = {};
+    void registerBuss(Buss* buss);
+    bool nmi(); // Returns 1 if NMI is generated
 
 private:
+    uint8_t read(const uint16_t& address);
+    void write(const uint16_t& address, const uint8_t& data);
+    
+public:
+    uint32_t windowBuffer[256*240];
+    bool frameDone = false;
+
+private:
+    union {
+        struct {
+            uint8_t nametableAddr : 2;
+            uint8_t vramInc : 1;
+            uint8_t spritePatternAddr : 1;
+            uint8_t bgPatternTableAddr : 1;
+            uint8_t spriteSize : 1;
+            uint8_t masterSlave : 1;
+            uint8_t genNMI : 1;
+        };
+        uint8_t reg;
+    } PPUctrl;
+
+    union {
+        struct {
+            uint8_t greyscale : 1;
+            uint8_t showBgLeft : 1;
+            uint8_t showSprLeft : 1;
+            uint8_t showBg : 1;
+            uint8_t ShowSpr : 1;
+            uint8_t emphasizeRed : 1;
+            uint8_t emphasizeGreen : 1;
+            uint8_t emphasizeBlue : 1;
+        };
+        uint8_t reg;
+    } PPUmask;
+
+    union {
+        struct {
+            uint8_t unused : 5;
+            uint8_t spriteOveflow : 1;
+            uint8_t spriteZeroHit : 1;
+            uint8_t verticalBlank : 1;
+        };
+        uint8_t reg;
+    } PPUstatus;
+
+    uint16_t m_scanline;
+    uint16_t m_cycle;
+
+    uint16_t m_vramAddress;
+    uint16_t m_tvramAddress;
+
+    // Shift registers
+    uint16_t m_patternTableData;
+    uint8_t m_paletteAttrData;
+
+    uint16_t m_nametableOffset;
+    uint32_t m_pixelOffset;
+    bool m_nmi;
+
     Buss* m_buss;
+
+    friend class PPUregisters;
 };
 
 class PPUregisters : public BussDevice {
 public:
     PPUregisters(std::shared_ptr<PPU> ppu) : m_ppu(ppu) {}
 
-    virtual uint8_t read(const uint16_t& address) override {
-        return m_ppu->registers[address % 8];
-    }
-    virtual void write(const uint16_t& address, const uint8_t& data) override {
-        m_ppu->registers[address % 8] = data;
-    }
+    virtual uint8_t read(const uint16_t& address) override;
+    virtual void write(const uint16_t& address, const uint8_t& data) override;
+
     virtual uint16_t getSize() override { return 0x2000; }
 
 private:
     std::shared_ptr<PPU> m_ppu;
+    bool m_addressLatch = false;
+    uint8_t m_dataBuffer;
 };
